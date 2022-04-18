928a86ffb965f47b422c124dc3a8417585c39d72
Salvatore Spina <saske0109@gmail.com>
2022-04-18 09:48:14 +0200

    update headers

---
+++ 02_Pipex/libft/srcs/ft_printf/ptr.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ptr.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sspina <sspina@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/30 15:40:27 by sspina            #+#    #+#             */
+/*   Updated: 2022/04/18 09:46:51 by sspina           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "ft_printf.h"
+
+int	write_ptr(va_list *args)
+{
+	size_t	addr;
+	int		cnt;
+
+	cnt = 0;
+	cnt += write(1, "0x", 2);
+	addr = (size_t)va_arg(*args, void *);
+	cnt += ft_putnbr_base(addr, "0123456789abcdef");
+	return (cnt);
+}
